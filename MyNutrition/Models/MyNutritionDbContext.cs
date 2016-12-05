bd880920927ba3387d9775ee8dd634130a32af07
Mihail Mihaylov <mishomihaylov91@gmail.com>
2016-12-05 21:49:46 +0200

    Context and User models added

---
+++ MyNutrition/Models/MyNutritionDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MyNutrition.DataModels.Models;
+using System.Data.Entity;
+
+namespace MyNutrition.Models
+{
+
+    public class MyNutritionDbContext : IdentityDbContext<MyNutritionUser>
+    {
+        public MyNutritionDbContext()
+            : base("MyNutritionConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MyNutritionDbContext Create()
+        {
+            return new MyNutritionDbContext();
+        }
+
+        public DbSet<Calories> Calories { get; set; }
+
+        public DbSet<Carbohydrates> Carbohydrates { get; set; }
+
+        public DbSet<Fats> Fats { get; set; }
+
+        public DbSet<FattyAcids> FattyAcids { get; set; }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<IngredientType> IngredientType { get; set; }
+
+        public DbSet<Minerals> Minerals { get; set; }
+
+        public DbSet<Other> Other { get; set; }
+
+        public DbSet<Protein> Protein { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<RecipeType> RecipeTypes { get; set; }
+
+        public DbSet<ServingSize> ServingSize { get; set; }
+
+        public DbSet<Sterols> Sterols { get; set; }
+
+        public DbSet<Sugars> Sugars { get; set; }
+
+        public DbSet<Vitamins> Vitamins { get; set; }
+    }
+}
---
+++ MyNutrition/Models/MyNutritionUser.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MyNutrition.DataModels.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace MyNutrition.Models
+{
+    public class MyNutritionUser : IdentityUser
+    {
+        public MyNutritionUser() : base()
+        {
+            this.Recipes = new HashSet<Recipe>();
+        }
+
+        [StringLength(50)]
+        public string PersonName { get; set; }
+
+        [StringLength(50)]
+        public string Town { get; set; }
+
+        public string Address { get; set; }
+
+        public int Phone { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime CreatedOn { get; set; }
+
+        public ICollection<Recipe> Recipes { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<MyNutritionUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+}
