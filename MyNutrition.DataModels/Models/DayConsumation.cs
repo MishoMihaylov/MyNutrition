cbc2cb12a084983ef8bc2b2e69d5f1e18bc03c3b
Mihail Mihaylov <mishomihaylov91@gmail.com>
2016-12-11 19:49:14 +0200

    Model improvements

--- MyNutrition.DataModels/Models/DayConsumation.cs
+++ MyNutrition.DataModels/Models/DayConsumation.cs
@@ -8,17 +8,29 @@ namespace MyNutrition.DataModels.Models
     {
         public DayConsumation()
         {
-            this.ConsumedIngredients = new HashSet<DayConsumptionIngredient>();
-            this.ConsumedRecipes = new HashSet<DayConsumationRecipe>();
+            this.Ingredients = new HashSet<Ingredient>();
+            this.Recipes = new HashSet<Recipe>();
         }
 
         public int Id { get; set; }
 
-        [Index(IsUnique = true)]
-        public DateTime Date { get; set; }
+        [Index(IsUnique = true)] //To be made for date only
+        public virtual DateTime Date { get; set; }
 
-        public virtual ICollection<DayConsumptionIngredient> ConsumedIngredients { get; set; }
+        public virtual Protein DayProteins { get; set; }
 
-        public virtual ICollection<DayConsumationRecipe> ConsumedRecipes { get; set; }
+        public virtual Calories DayCalories { get; set; }
+
+        public virtual Carbohydrates DayCarbohydrates { get; set; }
+
+        public virtual Fats DayFats { get; set; }
+
+        public virtual Vitamins DayVitamins { get; set; }
+
+        public virtual Minerals DayMinerals { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+
+        public virtual ICollection<Recipe> Recipes { get; set; }
     }
 }
